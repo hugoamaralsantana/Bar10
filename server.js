56db5709adefb0a0ebe1cbeab6dad778b348d2d7
Victor Hugo Amaral Santana <victorhugoamaralsantana@gmail.com>
2021-01-18 13:44:00 -0300

    Big changes

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect Database
+connectDB();
+
+//Initialize Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('API Running');
 });
+
+//Define Routes Files
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/ingredients', require('./routes/api/ingredients'));
+app.use('/api/articles', require('./routes/api/articles'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/recipe', require('./routes/api/recipe'));
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 
